eef6a8b38fc4df2c0b75323ecd3c921716f8db99
Sebastian Fehrnström-Gröhn <sebastian@sebgrohn.se>
2025-03-12 08:46:10 +0100

    Make Vite config compatible with GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   test: {},
+  build: {
+    outDir: "docs", // For compatibility with GitHub Pages
+  },
+  base: "./", // For compatibility with GitHub Pages
 });
